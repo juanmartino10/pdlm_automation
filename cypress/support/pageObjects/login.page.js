b205ab6be58599b1f7022ce07c6c0ff9c0a7d57e
Juan Martino <martinojuan10@gmail.com>
2024-01-24 17:55:09 -0300

    login object updated

--- cypress/support/pageObjects/login.page.js
+++ cypress/support/pageObjects/login.page.js
@@ -1,34 +1,34 @@
 class LoginPage {
 
-  getAsideButton() {
+  get AsideButton() {
     return cy.get('#btnMainMenuMobile');
   }
 
-  getAccountButton() {
+  get AccountButton() {
     return cy.get('a.btnItem.btnMiCuenta');
   }
-    getEmailInput () {
+    get EmailInput () {
       return cy.get('input[name="txtEmail"]');
     }
   
-    getPasswordInput () {
+    get PasswordInput () {
       return cy.get('input[name="txtPassword"]');
     }
   
-    getLoginButton() {
+    get LoginButton() {
       return cy.get('button.btnIngresar');
     }
 
-    getMessageError() {
+    get MessageError() {
       return cy.get('div.msg.err p')
     }
   
     login(email, password) {
-      this.getAsideButton().click()
-      this.getAccountButton().click({ force: true, multiple: true });
-      this.getEmailInput().type(email)
-      this.getPasswordInput().type(password);
-      this.getLoginButton().click({ multiple: true });
+      this.AsideButton.click()
+      this.AccountButton.click({ force: true, multiple: true });
+      this.EmailInput.type(email)
+      this.PasswordInput.type(password);
+      this.LoginButton.click({ multiple: true });
     }
   }
   
